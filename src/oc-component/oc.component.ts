86eb941ed8e064a7ed97a219a6084a952ed55805
lacant <thibaud.lacan@guestline.com>
2019-07-10 12:03:11 +0100

    Fix OC init to render even when OC is not present yet

--- src/oc-component/oc.component.ts
+++ src/oc-component/oc.component.ts
@@ -11,29 +11,34 @@ import { OcOptions } from './oc-options';
 
 @Component({
   selector: 'ngx-oc-component',
-  template: `<ng-template></ng-template>`
+  template: `
+    <ng-template></ng-template>
+  `
 })
 export class OcComponent implements OnInit {
   @Input() public options: OcOptions;
   @Output() public rendered: EventEmitter<any> = new EventEmitter();
 
-  constructor(private renderer: Renderer2,
-              private el: ElementRef) {}
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
 
   public ngOnInit(): void {
-    const oc = window['oc'];
-    if (!this.options || !oc) {
-      this.rendered.emit();
+    if (!this.options) {
+      console.error('Missing OcOptions');
       return;
     }
 
-    const content = oc.build(this.options);
-    const div = this.renderer.createElement('div');
-    this.renderer.setProperty(div, 'innerHTML', content);
-    this.renderer.appendChild(this.el.nativeElement, div);
-    oc.events.on('oc:rendered', (event, data) => {
-      this.rendered.emit({event, data});
+    window['oc'] = window['oc'] || {};
+    const oc = window['oc'];
+    oc.cmd = oc.cmd || [];
+    oc.cmd.push(function(oc) {
+      const content = oc.build(this.options);
+      const div = this.renderer.createElement('div');
+      this.renderer.setProperty(div, 'innerHTML', content);
+      this.renderer.appendChild(this.el.nativeElement, div);
+      oc.events.on('oc:rendered', (event, data) => {
+        this.rendered.emit({ event, data });
+      });
+      oc.renderUnloadedComponents();
     });
-    oc.renderUnloadedComponents();
   }
 }
